55f73fae502b292588f032c7379f727a054729f6
Aidyn Torgayev <aidyn.torgayev@gmail.com>
2021-08-17 20:40:47 +0600

    move mouse

--- client.go
+++ client.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"net/url"
 
+	"github.com/go-vgo/robotgo"
 	"github.com/gorilla/websocket"
+	hook "github.com/robotn/gohook"
 )
 
 func clientRun(address string) {
@@ -22,14 +24,40 @@ func clientRun(address string) {
 		_, bz, err := c.ReadMessage()
 		if err != nil {
 			log.Println("ReadMessage:", err)
-			return
+			continue
 		}
-		message, err := eventsFromBytesJSON(bz)
+		event, err := eventsFromBytesJSON(bz)
 		if err != nil {
 			log.Println("eventsFromBytes:", err)
-			return
+			continue
 		}
 
-		log.Printf("recv: %s", message)
+		if err := apply(event); err != nil {
+			log.Println(err)
+			continue
+		}
 	}
+}
+
+func apply(evt hook.Event) error {
+	switch evt.Kind {
+	// Button, Clicks, X, Y, Amount, Rotation and Direction
+	case hook.MouseDown,
+		hook.MouseUp,
+		hook.MouseHold,
+		hook.MouseDrag,
+		hook.MouseWheel:
+		// stub
+	case hook.MouseMove:
+		robotgo.MoveMouse(int(evt.X), int(evt.Y))
+
+		// Mask, Keycode, Rawcode, and Keychar,
+		// Keychar is probably what you want.
+	case hook.KeyDown,
+		hook.KeyHold,
+		hook.KeyUp:
+
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/adntgv/hidteleport
 go 1.16
 
 require (
+	github.com/go-vgo/robotgo v0.99.0
 	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/websocket v1.4.2
 	github.com/robotn/gohook v0.30.6
